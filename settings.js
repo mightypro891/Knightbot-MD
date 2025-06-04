b4bab192fcbab519650f19cbfa51ef72801a9c8f
mightypro891 <133176573+mightypro891@users.noreply.github.com>
2025-06-04 11:08:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Mighty Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "Mighty Bot",
+  botOwner: 'Mighty', // Your name
+  ownerNumber: '2349017458955', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
